13fc6544a8ad4aa3e3711489d388982c3a5d7733
LisaKr <lisa.kramarenko@gmail.com>
2019-03-20 11:22:46 +0100

    cleaned up the hooks

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -3,23 +3,18 @@ import axios from "axios";
 
 export const useAxios = url => {
   //in order to set state
-  const [quotes, setQuotes] = useState([]);
+  const [value, setValue] = useState([]);
   //function for fetching data, creating array of quotes and setting the state
   const getData = async () => {
     const { data } = await axios.get(url);
-
-    console.log(data);
+    //data comes back as an array, I want to have it as an object though
 
     let obj = {};
-
-    let allQuotes = [];
-
     await data.forEach(el => {
-      allQuotes = [...allQuotes, ...el.quotes];
       obj[el.name] = el.quotes;
     });
-
-    setQuotes(obj);
+    //setting it to state, so that there is a value to return
+    setValue(obj);
   };
 
   //useEffect cannot return a promise, only another function
@@ -28,5 +23,6 @@ export const useAxios = url => {
     // empty array tells useEffect to run only on mount and unmount
   }, []);
 
-  return quotes;
+  //value is not an empty array anymore, but it was set to state in the getData function
+  return value;
 };
--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,30 +1,30 @@
 import { useState, useEffect } from "react";
 
 export default function useFilter(raw, name) {
-  const [quotes, setQuotes] = useState([]);
+  const [value, setValue] = useState({});
 
+  //filtering object to only keep the key which is passed as name, empty object in the end is the initial value, callback is holding value for every iteration
   var filteredObject = Object.keys(raw).reduce(function(r, e) {
-    if (e == name) {
+    if (e === name) {
       r[e] = raw[e];
     }
     return r;
   }, {});
 
-  console.log("filt obj", filteredObject, filteredObject.length);
+  //every time the page re-renders because quotes or props name changed, this effect will run. if the filter yielded nothing (meaning everyone is selected, then the object is not filtered)
+  useEffect(
+    () => {
+      if (
+        Object.entries(filteredObject).length === 0 &&
+        filteredObject.constructor === Object
+      ) {
+        setValue(raw);
+      } else {
+        setValue(filteredObject);
+      }
+    },
+    [raw, name]
+  );
 
-  useEffect(() => {
-    if (
-      Object.entries(filteredObject).length === 0 &&
-      filteredObject.constructor === Object
-    ) {
-      setQuotes(raw);
-    } else {
-      setQuotes(filteredObject);
-    }
-  }, []);
-
-  return Object.entries(filteredObject).length === 0 &&
-    filteredObject.constructor === Object
-    ? raw
-    : filteredObject;
+  return value;
 }
